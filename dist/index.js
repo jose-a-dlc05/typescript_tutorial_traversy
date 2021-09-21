c57a85a3a80988feca061c81c81a2584ec049e90
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-09-21 02:32:22 -0400

    Worked up to arrays and tuples

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = 'Traversy Media';
+let isPublished = true;
+let x = 'Hello';
+let age = 31;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+// Tuple
+let person = [1, 'Brad', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+];
